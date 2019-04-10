59b6fbb6a4f5119708cf58e354b47e2eb94d8e7e
juncy <jyunkeeees724@gmail.com>
2019-04-10 22:35:02 +0900

    バイトサイズを調べる

---
+++ C/Sizeof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	printf("char型は%dバイトです\n",sizeof(char) );
+	printf("short型は%dバイトです\n", sizeof(short));
+	printf("int型は%dバイトです\n", sizeof(int));
+	printf("long型は%dバイトです\n", sizeof(long));
+
+	return (0);
+}
